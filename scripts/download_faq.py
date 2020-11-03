6d609c8bab52e4bd2056c62ba451b52ad875d4a3
CapBlood <stalker.anonim@mail.ru>
2020-11-03 20:31:26 +0300

    feat(scripts): реализован скрипт для скачивания FAQ

--- scripts/download_faq.py
+++ scripts/download_faq.py
@@ -4,6 +4,7 @@
 import lxml.html
 import requests
 from fake_useragent import UserAgent
+import pandas as pd
 
 
 def download_ml():
@@ -11,15 +12,34 @@ def download_ml():
     с https://machinelearningmastery.com/faq/.
     """
 
-    ua_str = UserAgent().chrome
+    ua_str = UserAgent().firefox
     url = 'https://machinelearningmastery.com/faq/'
     r = requests.get(url, headers={"User-Agent": ua_str})
     html_code = r.content
     tree = lxml.html.fromstring(html_code)
-    el = tree.xpath("//div[@class='ufaq-faq-list']")
-    print(el[0])
+    questions = tree.xpath("//div[@class='ufaq-faq-title ufaq-faq-toggle']")
+    answers = tree.xpath("//div[@class='ewd-ufaq-post-margin ufaq-faq-post']")
+    return questions, answers
 
+
+def toDataset(q, a, path):
+    """Создание csv из FAQ.
+
+    Args:
+        q (list): вопросы из FAQ.
+        a (list): ответы из FAQ.
+        path (str): путь для csv.
+    """
+
+    col1 = [''.join(i.itertext()) for i in q]
+    col2 = [''.join(i.itertext()) for i in a]
+    df = pd.DataFrame(data={'questions': col1, 'answers': col2})
+    df.to_csv(path, index=False)
 
 
 if __name__ == '__main__':
-    download_ml()
+    q, a = download_ml()
+    toDataset(q, a, '../datasets/example.csv')
+
+
+
