42e3f01afa516bbabf4c3500e518b303ca81cf95
CapBlood <stalker.anonim@mail.ru>
2020-11-12 22:24:08 +0300

    feat(dataset): вынесена загрузка датасета

--- chillerbot/__main__.py
+++ chillerbot/__main__.py
@@ -1,9 +1,30 @@
+import os
+
+from pandas import read_csv
+
 from chillerbot.handlers import HandlerFaq, HandlerMock
 from chillerbot.bot import BotHttp, BotVk
+from chillerbot.intent import buildModel
+from chillerbot.constants import THRESHOLD
+
+
+def getDataset():
+    try:
+        path = os.environ['PATH_DATASET']
+        faq = read_csv(path)
+    except KeyError:
+        raise EnvironmentError("PATH_DATASET: path to dataset is not specified")
+    except FileNotFoundError:
+        raise FileNotFoundError("Dataset not found: {}".format(path))
+    return faq
 
 
 if __name__ == '__main__':
-    handler = HandlerFaq()
+    df = getDataset()
+    model = buildModel()
+    model.fit(df['questions'].to_numpy(), df['answers'].to_numpy())
+
+    handler = HandlerFaq(model, lambda proba: 1 - proba > THRESHOLD)
     handler.set_next(HandlerMock())
     # bot = BotVk('48d58aaf62f57f5a6738b1b81b7663694369c27b7d2eb4d90db67b3b1fd547781aa3db065a7f2ab380a68',
     #             handler)
