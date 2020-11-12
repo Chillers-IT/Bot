67a98b693f5b390ea38ef15336e1e11e63f4a735
CapBlood <stalker.anonim@mail.ru>
2020-11-12 22:22:30 +0300

    feat(setup): добавлены все зависимости для установки

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 from setuptools import setup
+from setuptools.command import install, develop
+
+
+class PostInstallCommand(install):
+    def run(self):
+        import nltk
+        install.run(self)
+        nltk.download('stopwords')
+
+
+class PostDevelopCommand(develop):
+    def run(self):
+        import nltk
+        develop.run(self)
+        nltk.download('stopwords')
+
 
 setup(name='chillerbot',
       version='0.1',
       packages=['chillerbot'],
-      install_requires=['vkbottle', 'aiohttp'])
+      install_requires=['vkbottle', 'aiohttp', 'nltk', 'scikit-learn',
+                        'pandas', 'pymorphy2'],
+      cmd_class={
+            'develop': PostDevelopCommand,
+            'install': PostInstallCommand
+      })
